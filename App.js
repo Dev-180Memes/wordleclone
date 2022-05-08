e344fb99df881dfaf8ad345c3f50af23c6cd275a
Dev-180Memes <adeoluwaagbakosi@gmail.com>
2022-05-08 22:40:56 +0000

    Custom Keyboard Built

--- App.js
+++ App.js
@@ -1,20 +1,103 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, ScrollView } from 'react-native';
+import { colors } from './src/constants';
+import Keyboard from './src/components/Keyboard';
+
+const NUMBER_OF_TRIES = 6;
+
+const copyArray = (arr) => {
+  return [...arr.map((rows) => [...rows])];
+}
 
 export default function App() {
+  const word = "hello";
+  const letters = word.split('');
+
+  const [rows, setRows] = useState(
+    new Array(NUMBER_OF_TRIES).fill(new Array(letters.length).fill(''))
+  );
+  const [curRow, setCurRow] = useState(0);
+  const [curCol, setCurCol] = useState(0);
+
+  const onKeyPressed = (key) => {
+    const updatedRows = copyArray(rows);
+    updatedRows[curRow][curCol] = key;
+    setRows(updatedRows);
+    setCurCol(curCol + 1);
+  };
+
+  const isCellActive = (row, col) => {
+    return row === curRow && col === curCol;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="light" />
+
+      <Text style={styles.title}>WORDLE</Text>
+
+      <ScrollView style={styles.map}>
+        {rows.map((row, i) => (
+          <View key={`row-${i}`} style={styles.row}>
+            {row.map((cell, j) => (
+              <View
+                key={`cell-${i}-${j}`}
+                style={[
+                  styles.cell, 
+                  { borderColor: isCellActive(i, j) 
+                    ? colors.grey 
+                    : colors.darkgrey 
+                  },
+                ]}
+              >
+                <Text style={style.cellText}>{cell.toUpperCase()}</Text>
+              </View>
+            ))}
+          </View>
+        ))}
+      </ScrollView>
+
+      <Keyboard onKeyPressed={onKeyPressed} />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.black,
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  title: {
+    color: colors.lightgrey,
+    fontSize: 32,
+    fontWeight: "bold",
+    letterSpacing: 7,
+  },
+
+  map: {
+    alignSelf: "stretch",
+    marginVertical: 20,
+  },
+  row: {
+    alignSelf: "stretch",
+    flexDirection: "row",
+    justifyContent: "center"
+  },
+  cell: {
+    borderWidth: 3,
+    borderColor: colors.darkgrey,
+    flex: 1,
+    maxWidth: 70,
+    aspectRatio: 1,
+    margin: 3,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cellText: {
+    color: colors.lightgrey,
+    fontWeight: "bold",
+    fontSize: 28,
   },
 });
