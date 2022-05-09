e739a1106538c6f0bb613fcced67c01f0d2d3a94
Dev-180Memes <adeoluwaagbakosi@gmail.com>
2022-05-09 13:36:56 +0000

    added clear and enter functionality

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, SafeAreaView, ScrollView, View } from 'react-native';
-import { colors } from './src/constants';
+import { colors, CLEAR, ENTER } from './src/constants';
 import Keyboard from './src/components/Keyboard';
 
 const NUMBER_OF_TRIES = 6;
@@ -22,9 +22,30 @@ export default function App() {
 
   const onKeyPressed = (key) => {
     const updatedRows = copyArray(rows);
-    updatedRows[curRow][curCol] = key;
-    setRows(updatedRows);
-    setCurCol(curCol + 1);
+
+    if (key === CLEAR) {
+      const prevCol = curCol - 1;
+      if (prevCol >= 0) {
+        updatedRows[curRow][prevCol] = "";
+        setRows(updatedRows);
+        setCurCol(prevCol);
+      }
+      return;
+    }
+
+    if (key === ENTER) {
+      if (curCol === rows[0].length) {
+        setCurRow(curRow + 1);
+        setCurCol(0);
+      }
+      return;
+    }
+
+    if (curCol < rows.length) {
+      updatedRows[curRow][curCol] = key;
+      setRows(updatedRows);
+      setCurCol(curCol + 1);
+    }
   };
 
   const isCellActive = (row, col) => {
