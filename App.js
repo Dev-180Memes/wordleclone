52edbcd744f8071897181e115cb25a58e120ffd7
Dev-180Memes <adeoluwaagbakosi@gmail.com>
2022-05-11 21:55:39 +0000

    refactor application

--- App.js
+++ App.js
@@ -1,116 +1,17 @@
-import { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, SafeAreaView, ScrollView, View } from 'react-native';
-import { colors, CLEAR, ENTER } from './src/constants';
-import Keyboard from './src/components/Keyboard';
-
-const NUMBER_OF_TRIES = 6;
-
-const copyArray = (arr) => {
-  return [...arr.map((rows) => [...rows])];
-}
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, SafeAreaView } from "react-native";
+import { colors } from "./src/constants";
+import Game from './src/components/Game';
 
 export default function App() {
-  const word = "hello";
-  const letters = word.split('');
-
-  const [rows, setRows] = useState(
-    new Array(NUMBER_OF_TRIES).fill(new Array(letters.length).fill(''))
-  );
-  const [curRow, setCurRow] = useState(0);
-  const [curCol, setCurCol] = useState(0);
-
-  const onKeyPressed = (key) => {
-    const updatedRows = copyArray(rows);
-
-    if (key === CLEAR) {
-      const prevCol = curCol - 1;
-      if (prevCol >= 0) {
-        updatedRows[curRow][prevCol] = "";
-        setRows(updatedRows);
-        setCurCol(prevCol);
-      }
-      return;
-    }
-
-    if (key === ENTER) {
-      if (curCol === rows[0].length) {
-        setCurRow(curRow + 1);
-        setCurCol(0);
-      }
-      return;
-    }
-
-    if (curCol < rows.length) {
-      updatedRows[curRow][curCol] = key;
-      setRows(updatedRows);
-      setCurCol(curCol + 1);
-    }
-  };
-
-  const isCellActive = (row, col) => {
-    return row === curRow && col === curCol;
-  };
-
-  const getCellBGColor = (row, col) => {
-    const letter = rows[row][col];
-
-    if (row >= curRow) {
-      return colors.black;
-    }
-    if (letter === letters[col]) {
-      return colors.primary;
-    }
-    if (letters.includes(letter)) {
-      return colors.secondary;
-    }
-    return colors.darkgrey
-  }
-
-  const getAllLettersWithColor = (color) => {
-    return rows.flatMap((row, i) =>
-      row.filter((cell, j) => getCellBGColor(i, j) === color)
-    );
-  };
-
-  const greenCaps = getAllLettersWithColor(colors.primary);
-  const yellowCaps = getAllLettersWithColor(colors.secondary);
-  const greyCaps = getAllLettersWithColor(colors.darkgrey);
-
+  
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="light" />
 
       <Text style={styles.title}>WORDLE</Text>
 
-      <ScrollView style={styles.map}>
-        {rows.map((row, i) => (
-          <View key={`row-${i}`} style={styles.row}>
-            {row.map((letter, j) => (
-              <View
-                key={`cell-${i}-${j}`}
-                style={[
-                  styles.cell, 
-                  { borderColor: isCellActive(i, j) 
-                      ? colors.grey 
-                      : colors.darkgrey,
-                    backgroundColor: getCellBGColor(i, j),
-                  },
-                ]}
-              >
-                <Text style={styles.cellText}>{letter.toUpperCase()}</Text>
-              </View>
-            ))}
-          </View>
-        ))}
-      </ScrollView>
-
-      <Keyboard 
-        onKeyPressed={onKeyPressed} 
-        greenCaps={greenCaps}
-        yellowCaps={yellowCaps}
-        greyCaps={greyCaps}
-      />
+      <Game />
     </SafeAreaView>
   );
 }
@@ -119,37 +20,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.black,
-    alignItems: 'center',
+    alignItems: "center",
   },
   title: {
     color: colors.lightgrey,
     fontSize: 32,
     fontWeight: "bold",
     letterSpacing: 7,
-  },
-
-  map: {
-    alignSelf: "stretch",
-    marginVertical: 20,
-  },
-  row: {
-    alignSelf: "stretch",
-    flexDirection: "row",
-    justifyContent: "center"
-  },
-  cell: {
-    borderWidth: 3,
-    borderColor: colors.darkgrey,
-    flex: 1,
-    maxWidth: 70,
-    aspectRatio: 1,
-    margin: 3,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  cellText: {
-    color: colors.lightgrey,
-    fontWeight: "bold",
-    fontSize: 28,
   },
 });
